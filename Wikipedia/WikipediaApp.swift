de2f57e12f57da58dcbd9fa221837f41182f9eb3
Carmine Porricelli <carmineporricelli96@gmail.com>
2021-12-06 22:57:00 +0100

    Initial Commit

---
+++ Wikipedia/WikipediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  WikipediaApp.swift
+//  Wikipedia
+//
+//  Created by Carmine Porricelli on 06/12/21.
+//
+
+import SwiftUI
+
+@main
+struct WikipediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
